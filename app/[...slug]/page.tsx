94733fa3069bc6816806c1b119733ff878d158c3
rubenvangrins <ruben@gewest13.nl>
2022-11-04 13:48:10 +0100

    test: new staticpagegenerator

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,13 +1,15 @@
 import { fetchAPI } from '../../lib/fetch-client';
-import { /* getAllSlugs, */ getCurrentSlug } from '../../lib/query/pages.data';
+import { getAllSlugs, getCurrentSlug } from '../../lib/query/pages.data';
 
-// export async function generateStaticParams() {
-//   const { pages } = await fetchAPI(getAllSlugs);
+export async function generateStaticParams() {
+  const { pages } = await fetchAPI(getAllSlugs);
 
-//   return pages.nodes.map(({ uri }: { uri: string }) => ({
-//     slug: uri === '/' ? [''] : uri.split('/').filter((item) => item),
-//   }));
-// }
+  const filteredPages = pages.nodes.filter((page: any) => page.uri !== '/');
+
+  return filteredPages.map(({ uri }: { uri: string }) => ({
+    slug: uri === '/' ? [''] : uri.split('/').filter((item) => item),
+  }));
+}
 
 export default async function Page({ params }: { params: { slug: string[] } }) {
   const { page } = await fetchAPI(getCurrentSlug, { id: !params.slug ? '/' : params.slug.join('/') });
