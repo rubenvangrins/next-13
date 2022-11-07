536a4b42b08e59925df58724c44944d519325948
rubenvangrins <ruben@gewest13.nl>
2022-11-07 15:28:33 +0100

    test: no staticParams

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,30 +1,32 @@
 import { fetchAPI } from '../../lib/fetch-client';
-import { getAllSlugs, getPageData } from '../../lib/query/pages.data';
+import { getPageData } from '../../lib/query/pages.data';
 import Components from '../../src/shared/Components';
 import { PageInterface } from '../page';
 
-export async function generateStaticParams() {
-  const { pages } = await fetchAPI(getAllSlugs);
+// export async function generateStaticParams() {
+//   const { pages } = await fetchAPI(getAllSlugs);
 
-  return pages.nodes
-    .filter(({ uri }: { uri: string }) => uri !== '/')
-    .map(({ uri }: { uri: string }) => ({
-      slug: uri.split('/').filter((item) => item),
-    }));
-}
+//   return pages.nodes
+//     .filter(({ uri }: { uri: string }) => uri !== '/')
+//     .map(({ uri }: { uri: string }) => ({
+//       slug: uri.split('/').filter((item) => item),
+//     }));
+// }
 
 export default async function Page({ params }: { params: { slug: string[] } }) {
-  const { page } = await fetchAPI(getPageData, { id: !params.slug ? '/' : params.slug.join('/') }) as PageInterface;
+  const pageSlug = params.slug.join('/');
+
+  const { page } = await fetchAPI(getPageData, { id: !params.slug ? '/' : pageSlug }) as PageInterface;
 
   if (!page) {
     return (<h1>404</h1>);
   }
 
-  const { acfComponents: { components }, contentType: { node: { graphqlSingleName: postType } } } = page;
+  const { title, acfComponents: { components }, contentType: { node: { graphqlSingleName: postType } } } = page;
 
   return (
     <>
-      <h1>{page.title && page.title}</h1>
+      <h1>{title && title}</h1>
       <Components postType={postType} components={components} />
     </>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { ComponentInterface } from '../declare/global.components';
 import { fetchAPI } from '../lib/fetch-client';
 import { getPageData } from '../lib/query/pages.data';
-import Components from '../src/shared/Components';
 
 export interface PageInterface {
   page: {
@@ -26,12 +25,7 @@ export default async function Page() {
     return (<h1>404</h1>);
   }
 
-  const { acfComponents: { components }, contentType: { node: { graphqlSingleName: postType } } } = page;
-
   return (
-    <>
-      <h1>{page.title && page.title}</h1>
-      <Components postType={postType} components={components} />
-    </>
+    <h1>{page.title && page.title}</h1>
   );
 }
