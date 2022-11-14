8086e559cbcf95f0b1e124b0209e63d859504d9a
rubenvangrins <ruben@gewest13.nl>
2022-11-14 10:46:52 +0100

    fix: remove styled types

--- src/shared/Richtext.tsx
+++ src/shared/Richtext.tsx
@@ -16,13 +16,21 @@ type RefType = HTMLDivElement;
 const Richtext = forwardRef<RefType, RichtTextInterface>((props, ref?) => {
   const { children, className, desktopMargin, mobileMargin } = props;
 
-  console.log(desktopMargin, mobileMargin);
-
   return (
     <div
       className={`richtext ${className}`}
       ref={ref}
       dangerouslySetInnerHTML={{ __html: children }}
+      style={{
+        '--desktop-margin-top': desktopMargin ? desktopMargin[0] : 0,
+        '--desktop-margin-right': desktopMargin ? desktopMargin[1] : 0,
+        '--desktop-margin-bottom': desktopMargin ? desktopMargin[2] : 0,
+        '--desktop-margin-left': desktopMargin ? desktopMargin[3] : 0,
+        '--mobile-margin-top': mobileMargin ? mobileMargin[0] : 0,
+        '--mobile-margin-right': mobileMargin ? mobileMargin[1] : 0,
+        '--mobile-margin-bottom': mobileMargin ? mobileMargin[2] : 0,
+        '--mobile-margin-left': mobileMargin ? mobileMargin[3] : 0,
+      }}
     />
   );
 });
